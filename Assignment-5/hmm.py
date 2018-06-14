a3530567e4d668413a28ee0c2e3076515adf9a22
Zejun Liu <zejunliu@usc.edu>
2018-06-14 16:52:17 -0700

    Add files via upload

---
+++ Assignment-5/hmm.py
@@ -0,0 +1,190 @@
+from __future__ import print_function
+import json
+import numpy as np
+import sys
+
+def forward(pi, A, B, O):
+  """
+  Forward algorithm
+
+  Inputs:
+  - pi: A numpy array of initial probailities. pi[i] = P(Z_1 = s_i)
+  - A: A numpy array of transition probailities. A[i, j] = P(Z_t = s_j|Z_t-1 = s_i)
+  - B: A numpy array of observation probabilities. B[i, k] = P(X_t = o_k| Z_t = s_i)
+  - O: A list of observation sequence (in terms of index, not the actual symbol)
+
+  Returns:
+  - alpha: A numpy array alpha[j, t] = P(Z_t = s_j, x_1:x_t)
+  """
+  S = len(pi)
+  N = len(O)
+  alpha = np.zeros([S, N])
+  ###################################################
+  # Q3.1 Edit here
+  ###################################################
+  ###compute the initial alpha
+  for i in range(S):
+    alpha[i,0] = pi[i] * B[i,O[0]]
+  for i in range(1, N): 
+    for j in range(S):
+      sum = 0
+      for k in range(S):
+        sum += alpha[k, i -1]* A[k, j]
+      sum *= B[j,O[i]]
+      alpha[j,i] = sum
+  return alpha
+
+
+def backward(pi, A, B, O):
+  """
+  Backward algorithm
+
+  Inputs:
+  - pi: A numpy array of initial probailities. pi[i] = P(Z_1 = s_i)
+  - A: A numpy array of transition probailities. A[i, j] = P(Z_t = s_j|Z_t-1 = s_i)
+  - B: A numpy array of observation probabilities. B[i, k] = P(X_t = o_k| Z_t = s_i)
+  - O: A list of observation sequence (in terms of index, not the actual symbol)
+
+  Returns:
+  - beta: A numpy array beta[j, t] = P(Z_t = s_j, x_t+1:x_T)
+  """
+  S = len(pi)
+  N = len(O)
+  beta = np.zeros([S, N])
+  ###################################################
+  # Q3.1 Edit here
+  ###################################################
+  for i in range(S):
+    beta[i,N-1] = 1
+  for i in range(N-2, -1, -1): 
+    for j in range(S):
+      sum = 0
+      for k in range(S):
+        sum += beta[k, i + 1]* A[j, k] * B[k,O[i+1]]
+      beta[j,i] = sum
+  return beta
+
+def seqprob_forward(alpha):
+  """
+  Total probability of observing the whole sequence using the forward algorithm
+
+  Inputs:
+  - alpha: A numpy array alpha[j, t] = P(Z_t = s_j, x_1:x_t)
+
+  Returns:
+  - prob: A float number of P(x_1:x_T)
+  """
+  prob = 0
+  ###################################################
+  # Q3.2 Edit here
+  ###################################################
+  for i in range(len(alpha)):
+    prob += alpha[i,len(alpha[i]) - 1]
+  return prob
+
+
+def seqprob_backward(beta, pi, B, O):
+  """
+  Total probability of observing the whole sequence using the backward algorithm
+
+  Inputs:
+  - beta: A numpy array beta: A numpy array beta[j, t] = P(Z_t = s_j, x_t+1:x_T)
+  - pi: A numpy array of initial probailities. pi[i] = P(Z_1 = s_i)
+  - B: A numpy array of observation probabilities. B[i, k] = P(X_t = o_k| Z_t = s_i)
+  - O: A list of observation sequence
+      (in terms of the observation index, not the actual symbol)
+
+  Returns:
+  - prob: A float number of P(x_1:x_T)
+  """
+  prob = 0
+  ###################################################
+  # Q3.2 Edit here
+  ###################################################
+  for i in range(len(pi)):
+    prob += beta[i, 0] * pi[i] * B[i,O[0]]
+  return prob
+
+def viterbi(pi, A, B, O):
+  """
+  Viterbi algorithm
+
+  Inputs:
+  - pi: A numpy array of initial probailities. pi[i] = P(Z_1 = s_i)
+  - A: A numpy array of transition probailities. A[i, j] = P(Z_t = s_j|Z_t-1 = s_i)
+  - B: A numpy array of observation probabilities. B[i, k] = P(X_t = o_k| Z_t = s_i)
+  - O: A list of observation sequence (in terms of index, not the actual symbol)
+
+  Returns:
+  - path: A list of the most likely hidden state path k* (in terms of the state index)
+    argmax_k P(s_k1:s_kT | x_1:x_T)
+  """
+  path = []
+  ###################################################
+  # Q3.3 Edit here
+  ###################################################
+  N = len(O)
+  state_one = []
+  state_two = []
+  ini_state_one = pi[0] * B[0, O[0]]
+  ini_state_two = pi[1] * B[1, O[0]]
+  state_one.append(ini_state_one)
+  state_two.append(ini_state_two)
+  for i in range(1,N):
+  	a = state_one[-1] * A[0,0] * B[0,O[i]]
+  	b = state_two[-1] * A[1,0] * B[0,O[i]]
+  	if a > b:
+  		state_one.append(a)
+  	else:
+  		state_one.append(b)
+
+  	a = state_one[-1] * A[0,1] * B[1,O[i]]
+  	b = state_two[-1] * A[1,1] * B[1,O[i]]
+  	if a > b:
+  		state_two.append(a)
+  	else:
+  		state_two.append(b)
+  for i in range(len(state_one)):
+  	if state_one[i] > state_two[i]:
+  		path.append(0)
+  	else:
+  		pate.append(1)
+
+
+  return path
+
+
+##### DO NOT MODIFY ANYTHING BELOW THIS ###################
+def main():
+  model_file = sys.argv[1]
+  Osymbols = sys.argv[2]
+
+  #### load data ####
+  with open(model_file, 'r') as f:
+    data = json.load(f)
+  A = np.array(data['A'])
+  B = np.array(data['B'])
+  pi = np.array(data['pi'])
+  #### observation symbols #####
+  obs_symbols = data['observations']
+  #### state symbols #####
+  states_symbols = data['states']
+
+  N = len(Osymbols)
+  O = [obs_symbols[j] for j in Osymbols]
+
+  alpha = forward(pi, A, B, O)
+  beta = backward(pi, A, B, O)
+
+  prob1 = seqprob_forward(alpha)
+  prob2 = seqprob_backward(beta, pi, B, O)
+  print('Total log probability of observing the sequence %s is %g, %g.' % (Osymbols, np.log(prob1), np.log(prob2)))
+
+  viterbi_path = viterbi(pi, A, B, O)
+
+  print('Viterbi best path is ')
+  for j in viterbi_path:
+    print(states_symbols[j], end=' ')
+
+if __name__ == "__main__":
+  main()
